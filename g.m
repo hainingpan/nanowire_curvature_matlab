b1f19dc46dd3fa6cd92ae49866e08660d2425008
Haining Pan <jack_pan1994@126.com>
2018-09-08 21:57:45 -0400

    .

--- g.m
+++ g.m
@@ -1,8 +1,11 @@
 function [vzset,d_en]=g(mu,delta,alpha)
-vzm=sqrt(mu^2+delta^2)*1.1;
-vzgrid=101;
-vzset=linspace(0,vzm,vzgrid);
-vzstep=vzset(2)-vzset(1);
+% vzm=sqrt(mu^2+delta^2)*1.1;
+vzm=0.7;
+% vzgrid=101;
+% vzset=linspace(0,vzm,vzgrid);
+% vzstep=vzset(2)-vzset(1);
+vzstep=0.01;
+vzset=0:vzstep:vzm;
 en=zeros(1,length(vzset));
 parfor i=1:length(vzset)
      warning('off','all');
--- plotg.m
+++ plotg.m
@@ -1,8 +1,9 @@
 mulist=0:.01:.4;
-en=zeros(length(mulist),100);
+vzlist=0:.01:.7;
+en=zeros(length(mulist),length(vzlist));
 parfor muindex=1:length(mulist)
     mu=mulist(muindex);
     [~,en2]=g(mu,.2,2);
     en(muindex,:)=en2;
 end
-save('en2.dat','en','-ascii')
+save('en.dat','en','-ascii')
